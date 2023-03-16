1c576058b3f614ae785082c8e3042363f23e7e9f
motafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-03-16 14:43:02 -0700

    my first c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A C program that prints with printf functio.
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
