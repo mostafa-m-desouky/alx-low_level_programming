38c16c95ddea1597a59e2b9c0507bebd7a210145
motafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-03-16 13:55:36 -0700

    Hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point*
+ *
+ * Description: A C porgram that prints with put functio 
+ * 
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
+}
