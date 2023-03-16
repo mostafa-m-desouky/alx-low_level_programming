d3078d958a294b9be858d48b82f6d7f4f335f484
motafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-03-16 14:01:49 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
 /**
  * main - Entry point*
  *
- * Description: A C porgram that prints with put functio 
- * 
+ * Description: A C porgram that prints with put functio
+ *
  * Return: Always 0 (Success)
 */
 int main(void)
