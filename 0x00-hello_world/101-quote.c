9e40b2ba28b72426e0d370646c32a9d07b75cd4c
motafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-03-17 15:38:57 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: print a qoute using write function
+ * size_t write(int fd, const void *buf, size_t count);
+ *
+ * Return: 1 (error).
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
