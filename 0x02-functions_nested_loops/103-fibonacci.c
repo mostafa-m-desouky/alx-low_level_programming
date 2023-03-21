a4ec4078cc5d72e70a1c4d171d5e173fd64c25c9
motafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-03-21 14:48:58 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -13,19 +13,19 @@
 
 int main(void)
 {
-	unsigned long a = 0, b = 1, sum;
+	unsigned long fib1  = 0, fib2 = 1, sum;
 	float total_sum;
 
 	while (1)
 	{
-		sum = a + b;
+		sum = fib1 + fib2;
 		if (sum > 4000000)
 			break;
 
 		if ((sum % 2) == 0)
 			total_sum += sum;
-		a = b;
-		b = sum;
+		fib1 = fib2;
+		fib2 = sum;
 	}
 	printf("$.of\n", total_sum);
 
