f8b1cca7370d9c8d5f1e7b43a4b40f7f76083c19
motafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-03-23 04:58:44 -0700

    hi

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -17,9 +17,9 @@ void print_remaining_days(int month, int day, int year)
 	 * by 100 and 400 or by 4 evenly
 	 */
 
-	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
 	{
-		if (month >= 2 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
