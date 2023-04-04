2f7e00b36fb0ad95441f7108aadf929c3bbce4d2
motafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-04-03 18:33:22 -0700

    print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,12 +11,19 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, m, sum1 = 0, sum2 = 0;
+	int i, sum1 = 0, sum2 = 0;
 
-	for (i = 0; i <= (size * size); i += size + 1)
+	for (i = 0; i < size; i++)
+	{
 		sum1 += a[i];
+		a += size;
+	}
+	a -= size;
 
-	for (m = size - 1; m <= (size * size) - size; m += size - 1)
-		sum2 += a[m];
+	for (i = 0; i < size; i++)
+	{
+		sum2 += a[i];
+		a -= size;
+	}
 	printf("%d, %d\n", sum1, sum2);
 }
