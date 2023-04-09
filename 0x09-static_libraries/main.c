19d919b70cdf56b4260171469a63a70f16bef2ae
motafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-04-09 08:17:50 -0700

    0x09

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,11 +1,5 @@
 #include "main.h"
 
-/**
- * main - Entry point
- *
- * Retrun: always 0.
- */
-
 int main(void)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
