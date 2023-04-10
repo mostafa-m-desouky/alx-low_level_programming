64005ee8b7d9b31794eca67ed8d936cbc331ae8a
motafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-04-10 15:05:46 -0700

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -12,7 +12,17 @@
 
 int main(int argc, char *argv[])
 {
-	(void)*argv[];
-	printf("%d\n", argc - 1);
+	int x;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (x = 0; *argv; x++, argv++)
+			;
+
+		printf("%d\n", x - 1);
+	}
+
 	return (0);
 }
