6977d569bac5e76c12290ddbb92ed13595524c42
motafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-04-17 12:29:18 -0700

    0x0D preprocessor

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,1 +1,6 @@
-#define PI 3.14159265359 
+#ifndef _PI_H
+#define _PI_H
+
+#define PI 3.14159265359
+
+#endif /* _PI_H */
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCRION_LIKE_MACRO_H
+#define _FUNCRION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < 0 ? (-(x)) : (X))
+
+#endif /* _FUNCRION_LIKE_MACRO_H */
