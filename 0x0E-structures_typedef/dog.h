32ef837eefb04f0ba69f8dcfb7312439f0f8f849
motafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-04-17 13:40:27 -0700

    h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,30 @@
 #ifndef FILE_DOG
 #define FILE_DOG
-
 /**
- * struct dog - a dog struct
- *
- * @name: dog's name
- * @age: dog's age
- * @owner: owner's name
- *
- * Return: Always 0
+ *struct dog - structure dog
+ *@name:pointer to char name's dog
+ *@age:float age's dog
+ *@owner:pointer to char owner's dog
  */
-
 struct dog
 {
-	char *name;
-	float age;
-	char *ownner;
-}
+char *name;
+float age;
+char *owner;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+int _strlen(char *s);
+
+char *_strcpy(char *dest, char *src);
+
+void free_dog(dog_t *d);
+
 #endif
