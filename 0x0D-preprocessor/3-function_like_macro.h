b1c246289f82c42dcd011ebdb9122441e2ce0e97
motafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-04-17 12:44:50 -0700

    change

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef _FUNCTION_LIKE_MACRO_H
 #define _FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x) < (0) ? -(x) : (X))
+#define ABS(x) ((x) < (0) ? (-(x)) : (X))
 
 #endif /* _FUNCRION_LIKE_MACRO_H */
