7344b884b6471d448b211b23cfdbd35b330aa25c
motafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-04-20 17:19:31 -0700

    task1

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -18,7 +18,7 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 
 	va_start(args, n);
-	for (int i = 0; i < n; i++)
+	for (unsigned int i = 0; i < n; i++)
 		sum += va_arg(args, int);
 	va_end(args);
 
