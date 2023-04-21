1b7f39f26713e5f67238aabffe28d6ceb3ca8ae4
motafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-04-20 17:13:43 -0700

    task 1

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums variable
+ *
+ * @n: int
+ * @...: integers
+ *
+ * Return: the integer sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int sum = 0;
+
+	if (!n)
+		return (0);
+
+	va_start(args, n);
+	for (int i = 0; i < n; i++)
+		sum += va_arg(args, int);
+	va_end(args);
+
+	return (sum);
+}
