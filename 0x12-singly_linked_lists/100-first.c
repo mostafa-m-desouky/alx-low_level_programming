73123469aebf22d3e90227a7f66cd9a05beac5e4
mostafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-04-27 07:07:59 -0700

    Linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void _constructor(void) __attribute__ ((constructor));
+
+/**
+ * _constructor - executes befor main()
+ *
+ * Return: void
+ */
+
+void _constructor(void)
+{
+	printf("You 're beat! and yet, you must allow,\n""I bore my house upon my back!\n");
+}
