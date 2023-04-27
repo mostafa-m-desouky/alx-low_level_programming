0d70cad23cf168d61eb9a30c0359c7b77b8e4cfe
mostafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-04-27 09:31:40 -0700

    f

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
-void _constructor(void) __attribute__ ((constructor));
-
 /**
- * _constructor - executes befor main()
- *
+ * bmain - function executed before main
  * Return: void
  */
-
-void _constructor(void)
+void __attribute__ ((constructor)) bmain()
 {
-	printf("You're beat! and yet, you must allow,\n""I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
