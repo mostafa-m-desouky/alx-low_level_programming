3d21edae094c105a939b7147436a3ec446f29e3a
mostafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-05-02 14:45:39 -0700

    print_list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t nums = 0;
+	size_t num = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		nums++;
+		num++;
 		h = h->next;
 	}
-	return (nums);
+
+	return (num);
 }
