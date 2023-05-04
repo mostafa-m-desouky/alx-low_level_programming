71c97fa5304499000fdf43dad70e1e3ff5f8b781
mostafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-05-04 14:15:32 -0700

    advanced

---
+++ 0x14-bit_manipulation/100-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	int n;
+
+	n = get_endianness();
+	if (n != 0)
+	{
+		printf("Little Endian\n");
+	}
+	else
+	{
+		printf("Big Endian\n");
+	}
+	return (0);
+}
