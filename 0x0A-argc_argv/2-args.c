6090c015f39c377bbb4416ef3faa4cb03ca72c44
mostafa magdy <123871642+mostafa-m-desouky@users.noreply.github.com>
2025-05-18 23:11:26 +0300

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * @argc: pointer
- * @argv: pointer
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
