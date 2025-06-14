0e06fd2e049393deb9e05266a1c0bf8e6d5aa413
mostafa magdy <123871642+mostafa-m-desouky@users.noreply.github.com>
2025-06-14 18:52:30 +0300

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,21 +8,15 @@
  *Return:the index of the first element
  */
 
-int int_index(int *array, int size, int (*cmp)(int))
-{
-int i;
+int int_index(int *array, int size, int (*cmp)(int)) {
+    int i;
 
-if (array && cmp)
-{
+    if (array && cmp)
+        for (i = 0; i < size; i++) {
+            if ((*cmp)(array[i]) != 0)
+                return (i);
+        }
 
-for (i = 0; i < size; i++)
-{
-
-if ((*cmp)(array[i]) != 0)
-return (i);
-
+    return (-1);
 }
 
-}
-return (-1);
-}
