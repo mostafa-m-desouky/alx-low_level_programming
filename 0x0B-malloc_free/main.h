9d5f97430b0dffc9b7d6a46fa473f2b60d863016
mostafa magdy <123871642+mostafa-m-desouky@users.noreply.github.com>
2025-07-12 15:17:45 +0300

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,28 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
-
-
-
-
 
 #endif
