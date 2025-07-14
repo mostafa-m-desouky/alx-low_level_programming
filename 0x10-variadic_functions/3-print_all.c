18266f4b1fc59cccddcfb331f8f44c4c411691c0
mostafa magdy <123871642+mostafa-m-desouky@users.noreply.github.com>
2025-07-14 22:20:35 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,46 +8,47 @@
  * Return: void
  */
 
-void print_all(const char * const format, ...)
-{
-va_list args;
-unsigned int i = 0, j, c = 0;
-char *str;
-const char t_arg[] = "cifs";
+void print_all(const char * const format, ...) {
+    va_list args;
+    unsigned int i = 0;
+    char *str;
+    char *sep = "";
 
-va_start(args, format);
-while (format && format[i])
-{
-j = 0;
-while (t_arg[j])
-{
-if (format[i] == t_arg[j] && c)
-{
-printf(", ");
-break;
-} j++;
-}
-switch (format[i])
-{
-case 'c':
-printf("%c", va_arg(args, int)), c = 1;
-break;
-case 'i':
-printf("%d", va_arg(args, int)), c = 1;
-break;
-case 'f':
-printf("%f", va_arg(args, double)), c = 1;
-break;
-case 's':
-str = va_arg(args, char *), c = 1;
-if (!str)
-{
-printf("(nil)");
-break;
-}
-printf("%s", str);
-break;
-} i++;
-}
-printf("\n"), va_end(args);
+    va_start(args, format);
+
+    while (format && format[i])
+    {
+        switch (format[i])
+        {
+            case 'c':
+                printf("%s%c", sep, va_arg(args, int));
+                break;
+
+            case 'i':
+                printf("%s%d", sep, va_arg(args, int));
+                break;
+
+            case 'f':
+                printf("%s%f", sep, va_arg(args, double));
+                break;
+
+            case 's':
+                str = va_arg(args, char *);
+                if (str == NULL)
+                    printf("%s(nil)", sep);
+                else
+                    printf("%s%s", sep, str);
+                break;
+
+            default:
+                i++;
+                continue;
+        }
+
+        sep = ", ";
+        i++;
+    }
+
+    va_end(args);
+    printf("\n");
 }
